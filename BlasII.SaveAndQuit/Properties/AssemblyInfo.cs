3fa429063fdcf46dc5991ef8135cc2fe16492d13
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-15 14:24:40 -0400

    core: update assembly info

--- BlasII.SaveAndQuit/Properties/AssemblyInfo.cs
+++ BlasII.SaveAndQuit/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.SaveAndQuit;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.SaveAndQuit.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "0.6.0")]
+[assembly: MelonOptionalDependencies("Modding API", "1.0.0")]
