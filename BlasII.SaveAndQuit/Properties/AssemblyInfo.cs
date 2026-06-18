7315db89cb1914104b41169d307c2915d40ade37
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-06-18 13:47:05 -0400

    core: upgrade libs and api

--- BlasII.SaveAndQuit/Properties/AssemblyInfo.cs
+++ BlasII.SaveAndQuit/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.SaveAndQuit;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.SaveAndQuit.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "1.0.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.1.0")]
